c0dd99e0c12fbf195cd1e93fbc786a31ab6a5428
MDubovac <mladendubovac98@gmail.com>
2020-11-27 22:10:28 +0100

    Finished With the Database class

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -31,4 +31,51 @@ class Database{
             echo $this->error;
         }
     }
+
+    public function query($sql){
+        $this->stmt = $this->dbh->prepare($sql);
+    }
+
+    public function bind($param, $value, $type = null){
+        if(is_null($type)){
+            switch(true){
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+
+        $this->stmt->bindValue($param, $value, $type);
+    }
+
+    // Execute a prepared statement
+    public function execute(){
+        return $this->stmt->execute();
+    }
+
+    // Get result set as an array of objects
+    public function resultSet(){
+        $this->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    // Fetch Single result
+    public function single(){
+        $this->execute();
+        return $this->stmt->fetch(PDO::FETCH_OBJ);
+    }
+
+    // Row Count
+    public function rowCount(){
+        return $this->stmt->rowCount();
+    }
+
 }
